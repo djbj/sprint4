840b508b54e32daf782d181d8cbe435dc24d16e5
Katarina Sjölund <katarina.sjolund@outlook.com>
2018-01-05 14:27:01 +0100

    footer update

--- code/src/components/footer/index.js
+++ code/src/components/footer/index.js
@@ -7,7 +7,7 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer-container">
-        <img id="footer-svg" src="/images/footer_hearts.svg" alt="footer hearts" />
+        <img id="footer-svg" src="/images/footer_green.svg" alt="footer hearts" />
       </div>
     )
   }
